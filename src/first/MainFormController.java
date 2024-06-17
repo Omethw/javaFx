29268374b1c8b8c3139e145d6336be38b898e866
Gunathilaka <anjurashmika188@gmail.com>
2024-06-17 11:42:01 +0530

    Print name on label when click on Button

--- src/first/MainFormController.java
+++ src/first/MainFormController.java
@@ -1,4 +1,15 @@
 package first;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class MainFormController {
+
+    public Label lblName;
+
+    public void clickMeOnAction(ActionEvent actionEvent) {
+
+        lblName.setText("Nimal Perera");
+
+    }
 }
