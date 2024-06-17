d395fdf5bbcbbc37fd92699b8056e9f1db7d6872
Gunathilaka <anjurashmika188@gmail.com>
2024-06-17 11:26:25 +0530

    Catch Fxml File

--- src/first/AppInitializer.java
+++ src/first/AppInitializer.java
@@ -3,6 +3,8 @@ package first;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.net.URL;
+
 public class AppInitializer extends Application {
     public static void main(String[] args) {
         launch(args);
@@ -11,5 +13,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        //Catch Fxml File
+        URL resource = getClass().getResource("MainForm.fxml");
     }
 }
