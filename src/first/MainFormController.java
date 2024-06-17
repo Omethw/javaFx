1a896db63abaa6533a8bad79670e4a80cf728acd
Gunathilaka <anjurashmika188@gmail.com>
2024-06-17 11:46:07 +0530

    Set value to label from Name text filed

--- src/first/MainFormController.java
+++ src/first/MainFormController.java
@@ -2,14 +2,17 @@ package first;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class MainFormController {
 
     public Label lblName;
+    public TextField txtName;
 
     public void clickMeOnAction(ActionEvent actionEvent) {
 
-        lblName.setText("Nimal Perera");
+        String name = txtName.getText();
+        lblName.setText(name);
 
     }
 }
