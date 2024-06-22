66c17b252516a79db929becf1fad71f646356109
Gunathilaka <anjurashmika188@gmail.com>
2024-06-22 10:41:51 +0530

    Print Hello when UI is loaded with Initialize method. This is old way

---
+++ src/KeyIdentify/MainFormController.java
@@ -0,0 +1,15 @@
+package KeyIdentify;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainFormController implements Initializable {
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Hello");
+    }
+}
