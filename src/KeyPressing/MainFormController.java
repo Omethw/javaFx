3caf9d07bf62878ae0a128330567117c150e0fb9
Gunathilaka <anjurashmika188@gmail.com>
2024-06-22 10:14:15 +0530

    added components into fxml file and created controllers

--- src/KeyPressing/MainFormController.java
+++ src/KeyPressing/MainFormController.java
@@ -1,5 +1,15 @@
 package KeyPressing;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
-    
+
+    public TextField txtKeyPress;
+    public Label lblKeyPressAnswer;
+
+    public void keyPressOnAction(ActionEvent actionEvent) {
+
+    }
 }
