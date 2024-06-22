00e965c366802ee3beb8dc9b1460be21c80afab5
Gunathilaka <anjurashmika188@gmail.com>
2024-06-22 10:56:05 +0530

    Added a Listener into text field

--- src/KeyIdentify/MainFormController.java
+++ src/KeyIdentify/MainFormController.java
@@ -1,13 +1,23 @@
 package KeyIdentify;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainFormController {
 
+    public TextField txtInput;
+    public TextField txtOutput;
+
     public void initialize(){
-        System.out.println("Hello");
+
+        //Add a Listener
+        txtInput.textProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("New Value : "+newValue);
+            System.out.println("Old Value : "+oldValue);
+        });
+
     }
 }
