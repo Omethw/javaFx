5068e3f3fadbe8efd355d62aa81d82ae47dbd023
Gunathilaka <anjurashmika188@gmail.com>
2024-06-22 11:12:38 +0530

    Load data to the combo box from ArrayList using for each

--- src/ComboBox/MainFormController.java
+++ src/ComboBox/MainFormController.java
@@ -2,17 +2,28 @@ package ComboBox;
 
 import javafx.scene.control.ComboBox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainFormController {
 
     public ComboBox<String> cmbLanguages;
 
+    ArrayList<String> languages = new ArrayList<>(
+            Arrays.asList("Java","Python","Javascript","Php","Ruby")
+    );
+
     public void initialize(){
 
-        cmbLanguages.getItems().add("Java");
+        /*cmbLanguages.getItems().add("Java");
         cmbLanguages.getItems().add("Python");
         cmbLanguages.getItems().add("Javascript");
         cmbLanguages.getItems().add("Php");
-        cmbLanguages.getItems().add("Ruby");
+        cmbLanguages.getItems().add("Ruby");*/
+
+        for (String temp:languages) {
+            cmbLanguages.getItems().add(temp);
+        }
     }
 
 }
