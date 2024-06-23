7a42802b5f85e23f56a59df0672e2d8389d2fb86
Gunathilaka <anjurashmika188@gmail.com>
2024-06-23 10:24:13 +0530

    check input value in list if already exists

--- src/List/MainFormController.java
+++ src/List/MainFormController.java
@@ -3,6 +3,7 @@ package List;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -12,12 +13,29 @@ public class MainFormController {
     public TextField txtOutput;
     public TextField textInput;
 
+
     ObservableList<String> oblist = FXCollections.observableArrayList();
 
     public void inputOnAction(ActionEvent actionEvent) {
 
-        oblist.add(textInput.getText());
+        String value = textInput.getText();
+
+        if (isExists(value)){
+            Alert alert = new Alert(Alert.AlertType.WARNING,"Already Exists");
+            alert.show();
+            return;
+        }
+        oblist.add(value);
         lstLanguages.setItems(oblist);
         textInput.clear();
+    }
+
+    private boolean isExists(String value) {
+        for (String temp : oblist) {
+            if (temp.equalsIgnoreCase(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
